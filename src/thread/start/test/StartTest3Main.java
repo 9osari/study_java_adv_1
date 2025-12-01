397eaf645f3f78997bf139425630c55949191efa
9osari <128437395+9osari@users.noreply.github.com>
2025-12-01 22:45:43 +0900

    feat: 스레드 생성과 실행 학습

---
+++ src/thread/start/test/StartTest3Main.java
@@ -0,0 +1,25 @@
+package thread.start.test;
+
+import static util.MyLogger.log;
+
+public class StartTest2Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new CounterThread());
+        thread.start();
+    }
+
+    static class CounterThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                for (int i = 1; i <= 5; i++) {
+                    Thread.sleep(1000);
+                    log("value: " + i);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
